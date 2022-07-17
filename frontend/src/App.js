26ba3d40ad624615f889fece8979208c1dd954f8
AJAYKUMAR-A-S <asajay1902@gmail.com>
2022-07-17 19:06:22 +0530

    share code on github

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Product from './components/Product';
+import data from './data';
+
+function App() {
+  return (
+    <div class="grid-container">
+      <header class="row">
+        <div>
+          <a class="brand" href="/">atoz</a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div>
+          <div class="row center">
+            {
+              data.products.map(product => (
+                <Product key={product._id} product={product}></Product>
+              ))
+            }
+          </div>
+        </div>
+      </main>
+      <footer class="row center">All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
